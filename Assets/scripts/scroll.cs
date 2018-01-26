e5c64145bfe456a9f2c420ab4661b995e4b28acb
Andrew Gonzales <andrew.al.gonzales@hotmail.com>
2018-01-25 19:58:45 -0800

    asset horde

---
+++ Assets/scripts/scroll.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scroll : MonoBehaviour {
+
+	public float speed = 0.5f;
+	
+	void Start(){
+	
+	}
+	
+	void Update(){
+		Vector2 offset = new Vector2(Time.time * speed, 0);
+		
+		GetComponent<Renderer>().material.mainTextureOffset = offset;
+	}
+	
+
+}
+
